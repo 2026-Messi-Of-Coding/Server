e8c64784842b66b8bae5450fecac45dc755203c3
KwonJH614 <madogkwon@gmail.com>
2026-03-13 22:13:13 +0900

    SETTING

---
+++ src/test/java/com/gbsw/messiofcoding/MessiOfCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbsw.messiofcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessiOfCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
